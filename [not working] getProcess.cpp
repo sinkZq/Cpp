f8bdc36204d9e5a9a98a4ac68f0b2a90cec8554a
sinkZq <121246278+sinkZq@users.noreply.github.com>
2024-02-20 11:37:10 +0100

    Update [not working] getProcess.cpp

--- [not working] getProcess.cpp
+++ [not working] getProcess.cpp
@@ -2,39 +2,49 @@
 #include <string>
 #include <Windows.h>
 #include <tlhelp32.h>
+#include <vector>
 using namespace std;
 
-HANDLE getProcess(PCSTR processName) 
+DWORD getProcessId(PCWSTR processName) // def func as DWORD since return val of func (processid) is a DWORD (32bit int)
 {
 	DWORD processId = 0;
-	PROCESSENTRY32 processEntry;
-	ZeroMemory(&processEntry, sizeof(processEntry));
-	processEntry.dwSize = sizeof(PROCESSENTRY32);
-	
-	HANDLE processSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); // get process list with snapshot
 
-	if (Process32First(processSnapshot, &processEntry)) 
+	if (hSnapshot != INVALID_HANDLE_VALUE)
 	{
-		while (Process32Next(processSnapshot, &processEntry))
+		PROCESSENTRY32 processEntry = {};
+		processEntry.dwSize = sizeof(processEntry);
+
+		if (Process32First(hSnapshot, &processEntry)) do // do = init a while loop
 		{
-			if (string(processEntry.szExeFile) == string(processName))
+			if (wcsstr(processEntry.szExeFile, processName)) // wcsstr is eq to string.find in lua(u)
 			{
-
+				processId = processEntry.th32ProcessID;
+				break;
 			}
-		}
+		} 
+		while (Process32Next(hSnapshot, &processEntry));
 	}
-
-	if (processId == 0)
+	else
 	{
-		return NULL;
-
+		std::cout << "process not found, returning processid as 0" << std::endl;
+		return processId;
 	}
 
-	return OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+	return processId;
 };
 
 int main()
 {
-	getProcess("ac_client.exe");
+	DWORD processInfo1 = getProcessId(L"EADesktop.exe");
+	DWORD processInfo2 = getProcessId(L"Agent.exe");
+
+	vector<DWORD> tProcessInfo = { processInfo1, processInfo2 }; // def vector to create tbl
+
+	for (int processId : tProcessInfo) // iterate through tbl as processId = v, works as in pairs or any other iter call in lua
+	{
+		std::cout << processId << " " << std::endl;
+	}
+
 	return 0;
 }
